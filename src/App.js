e345de99b2e6e2a6d371312444053475b25a6012
piyush <sainipiyus11111@gmail.com>
2023-02-16 17:18:05 +0530

    add some syles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Expense  from './component/hj';
+
+import ExpenseList from './component/Hj';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Expense>
-
-        </Expense>
+        <ExpenseList />
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/component/hj.js
+++ src/component/hj.js
@@ -1,19 +1,22 @@
+import './A.css';
 import React from 'react';
 
 function ExpenseList() {
   const expenses = [
-    { name: 'Food', cost: 10 },
-    { name: 'Petrol', cost: 100 },
-    { name: 'Movies', cost: 200 },
+    { name: 'Food', cost: 10, location: 'Restaurant' },
+    { name: 'Petrol', cost: 100, location: 'Gas Station' },
+    { name: 'Movies', cost: 200, location: 'Cinema' },
   ];
 
   return (
-    <div>
-      <h2>Expense Items</h2>
-      <ul>
+    <div className="expense-list">
+      <h2 className="expense-list__title">Expense Items</h2>
+      <ul className="expense-list__items">
         {expenses.map((expense, index) => (
-          <li key={index}>
-            {expense.name} Rs {expense.cost}
+          <li key={index} className="expense-list__item">
+            <div className="expense-list__item-name">{expense.name}</div>
+            <div className="expense-list__item-cost">{expense.cost} Rs</div>
+            <div className="expense-list__item-location">Location: {expense.location}</div>
           </li>
         ))}
       </ul>
@@ -22,3 +25,4 @@ function ExpenseList() {
 }
 
 export default ExpenseList;
+
