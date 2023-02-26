c10dbd356f3ffc3b9744316360dc176200c36553
piyush <sainipiyus11111@gmail.com>
2023-02-26 19:28:23 +0530

    code123

--- src/Expense/ExpenseDate.js
+++ src/Expense/ExpenseDate.js
@@ -1,10 +1,8 @@
 import '../UI/ExpenseDate.css'
-
 function ExpenseDate(props) {
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
-  
     return (
       <div className="expense-date">
         <div className="expense-date__month">{month}</div>
--- src/Expense/NewExpense.js
+++ src/Expense/NewExpense.js
@@ -1,26 +1,59 @@
 import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
-
+import ExpenseItem from './ExpenseItem';
+import  './ExpenseFilter.css'
 function ExpenseList() {
   const [expenses, setExpenses] = useState([]);
+  const [filteredExpenses, setFilteredExpenses] = useState([]);
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
       return [...prevExpenses, expense];
     });
   };
+  const filterExpensesHandler = (event) => {
+    const year = event.target.value;
+    const filteredExpenses = expenses.filter((expense) => {
+      return expense.date.getFullYear().toString() === year;
+    });
+    setFilteredExpenses(filteredExpenses);
+  };
 
   return (
     <div className="new-expense">
+      <div>
       <h1>My Expenses</h1>
       <ExpenseForm onSaveExpenseData={addExpenseHandler} />
-      {expenses.map((expense) => (
-        <div key={expense.date.getTime()}>
-          <h2>{expense.title}</h2>
-          <p>Amount: {expense.amount}</p>
-          <p>Date: {expense.date.toISOString()}</p>
-        </div>
-      ))}
+      <div className='.filter-container'>
+        <label>Filter by Year</label>
+        <select onChange={filterExpensesHandler}>
+          <option value="">All</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+        </select>
+      </div>
+      {filteredExpenses.length > 0
+        ? filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+              location={expense.location}
+            />
+          ))
+          
+        : expenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+              location={expense.location}
+            />
+          ))}
+    </div>
     </div>
   );
 }
