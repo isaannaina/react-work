b909af8f32fbd335e5ec7c174188a95de8b5286b
piyush <sainipiyus11111@gmail.com>
2023-03-04 14:06:14 +0530

    BARCHART

--- src/Expense/NewExpense.js
+++ src/Expense/NewExpense.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import ExpenseItem from './ExpenseItem';
+import Chart from './Chart';
 import './ExpenseFilter.css';
 
 function ExpenseList() {
   const [expenses, setExpenses] = useState([]);
   const [filteredExpenses, setFilteredExpenses] = useState([]);
+  const [selectedYear, setSelectedYear] = useState('');
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
@@ -15,19 +17,32 @@ function ExpenseList() {
 
   const filterExpensesHandler = (event) => {
     const year = event.target.value;
+    setSelectedYear(year);
     const filteredExpenses = expenses.filter((expense) => {
       return expense.date.getFullYear().toString() === year;
     });
     setFilteredExpenses(filteredExpenses);
   };
 
+  // Calculate the total expenses for each year
+  const expensesByYear = {};
+  expenses.forEach((expense) => {
+    const year = expense.date.getFullYear().toString();
+    if (!expensesByYear[year]) {
+      expensesByYear[year] = 0;
+    }
+    expensesByYear[year] += expense.amount;
+  });
+
+  const maxValue = Math.max(...Object.values(expensesByYear));
+
   return (
     <div className="new-expense">
       <h1>My Expenses</h1>
       <ExpenseForm onSaveExpenseData={addExpenseHandler} />
-      <div className=".filter-container">
+      <div className="filter-container">
         <label>Filter by Year</label>
-        <select onChange={filterExpensesHandler}>
+        <select value={selectedYear} onChange={filterExpensesHandler}>
           <option value="">All</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
@@ -47,18 +62,24 @@ function ExpenseList() {
         : expenses.length === 1 ? (
             <p>Only single Expense here. Please add more...</p>
           ) : (
-            expenses.map((expense) => (
-              <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-                location={expense.location}
-              />
-            ))
+            <>
+              {Object.entries(expensesByYear).map(([year, total]) => (
+                <Chart key={year} value={total} maxValue={maxValue} />
+              ))}
+              {expenses.map((expense) => (
+                <ExpenseItem
+                  key={expense.id}
+                  title={expense.title}
+                  amount={expense.amount}
+                  date={expense.date}
+                  location={expense.location}
+                />
+              ))}
+            </>
           )}
     </div>
   );
 }
 
 export default ExpenseList;
+
